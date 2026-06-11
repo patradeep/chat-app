85519332f50cdd063eec63cf483d68ea72be0ce8
patradeep <deepanjanpatra6@gmail.com>
2026-06-11 22:14:14 +0530

    fix cross error

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,7 +14,8 @@ const allowedOrigins = [
   'http://localhost:5173',
   'https://chat-1u7aq1ylt-patradeeps-projects.vercel.app',
   'https://chat-app-two-jade-56.vercel.app',
-  'https://chat-app-git-main-patradeeps-projects.vercel.app/signup'
+  'https://chat-app-git-main-patradeeps-projects.vercel.app/signup',
+  'https://chat-app-frontent.onrender.com'
 ];
 app.use(express.json());
 app.use(cookieParser());
